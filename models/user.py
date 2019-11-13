6a7b68facf44fed77d72def27b99af52158e9a64
vivianlorenaortiz <973@holbertonschool.com>
2019-11-14 00:27:24 +0100

    adding file

--- models/user.py
+++ models/user.py
@@ -12,7 +12,3 @@ class User(BaseModel):
     password = ""
     first_name = ""
     last_name = ""
-
-    def __init__(self, *args, **kwargs):
-        """the init constructor"""
-        super().__init__(**kwargs)
